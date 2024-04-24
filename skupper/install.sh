c4b663a7fcd5c209b8995f4fea88c1d2c55cd5cf
Philip Hayes <phayes@phayes-thinkpadp1gen5.remote.csb>
2024-04-24 15:08:33 -0400

    Adding bash sript to install skupper

--- skupper/install.sh
+++ skupper/install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Installing skupper"
+wget https://github.com/skupperproject/skupper/releases/download/1.5.3/skupper-cli-1.5.3-linux-amd64.tgz
+tar -xf skupper-cli-1.5.3-linux-amd64.tgz
+mkdir $HOME/bin
+mv skupper $HOME/bin
+export PATH=$PATH:$HOME/bin 
