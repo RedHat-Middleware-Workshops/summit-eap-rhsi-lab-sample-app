eae29d394b2f8cda7d593b6cd09019b550450e5c
Philip Hayes <phayes@phayes-thinkpadp1gen5.remote.csb>
2024-04-24 15:22:35 -0400

    Adding bash sript to install skupper

--- skupper/install.sh
+++ skupper/install.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 echo "Installing skupper cli"
 wget https://github.com/skupperproject/skupper/releases/download/1.5.3/skupper-cli-1.5.3-linux-amd64.tgz
 tar -xf skupper-cli-1.5.3-linux-amd64.tgz
