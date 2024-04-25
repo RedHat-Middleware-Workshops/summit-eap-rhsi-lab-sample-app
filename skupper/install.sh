6c00cce70f73f492dd2dbd717d623c97bfe25665
Philip Hayes <phayes@phayes-thinkpadp1gen5.remote.csb>
2024-04-25 08:50:05 -0400

    Removed skupper install script

--- skupper/install.sh
+++
@@ -1,4 +0,0 @@
-echo "Installing skupper cli"
-wget https://github.com/skupperproject/skupper/releases/download/1.5.3/skupper-cli-1.5.3-linux-amd64.tgz
-tar -xf skupper-cli-1.5.3-linux-amd64.tgz
-mv skupper $HOME/.local/bin
